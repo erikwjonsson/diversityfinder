fb81151ab48d75f235e86a076a6457ac1744107e
memesta <erikwjonsson@gmail.com>
2016-07-28 17:07:39 +0200

    First major re-factoring. Added solutions to deal with urls with no concept or entitiy keywords.

--- app/controllers/dataextracts_controller.rb
+++ app/controllers/dataextracts_controller.rb
@@ -3,21 +3,6 @@ class DataextractsController < ApplicationController
 	require 'json'
 	respond_to :html, :js
 
-	def index
-		@dataextracts = Dataextract.all
-
-		api_check = File.read("api_key.txt").length
-		if api_check == 40
-			@api_key_status = 'API key present! You can now submit links!'
-		else
-			@api_key_status = 'No API key present!'
-		end
-
-					#Difference.all.each do |p|
-					#	p.destroy
-					#end
-	end
-
 	def insert_apikey
 		a = params[:api_key].to_s.gsub '["', ''
 		@user_api_key = a.gsub '"]', ''
@@ -30,208 +15,63 @@ class DataextractsController < ApplicationController
 		redirect_to :controller => "dataextracts", :action => "index"
 	end
 
-	def compare
-		if params[:dataextract].nil?
-			@rebase_id = 1
-		else
-			@rebase_id = params[:dataextract].to_s[/\d+/].to_i
-		end
-
+	def index
+		@rebase_id = params[:dataextract] ? params[:dataextract].to_s[/\d+/].to_i : 1
 		@dataextracts = Dataextract.all
-			@master_array = Array.new
-			@dataextracts.each do |p|
-				if p.entities_list.present? && p.concepts_list.present? && p.url.present?
+		File.read("api_key.txt").length == 40 ? (@api_key_status = 'API key present! You can now submit links!') : (@api_key_status = 'No API key present!')
+	end
 
-					# GET CONCEPTS AND ENITITES FOR ONE 
-					# CONCEPTS
-					concept_array = Array.new
-					obj = JSON.parse(p.concepts_list)
-					@concepts = obj['concepts']
-					@concepts.each do |c|
-						text = c['text']
-						concept_array.push(text)
-					end
-
-
-					# ENTITIES
-					entity_array = Array.new
-					obj = JSON.parse(p.entities_list)
-					@entities = obj['entities']
-					@entities.each do |e|
-						 text = e['text']
-						entity_array.push(text)
-					end
-
-
-					url = Array.new
-					url.push(p.url)
-
-					data_destilled_array = Array.new
-					data_destilled_array.push(url, entity_array, concept_array)
-
-					
-					@master_array.push(data_destilled_array)
-
-						puts "----------VLADIMIR--------"
-						puts concept_array
-
-						puts "----------PUTIN--------"
-						puts entity_array
-				end
-			end
-
-			@master_array.each do |data_destilled_array| # Getting 
-				@base_concepts = data_destilled_array[2].count # a base is picked and it's entities counted
-				@base_array_concepts = data_destilled_array[2]
-				@base_entities = data_destilled_array[1].count # a base is picked and it's entities counted
-				@base_entities_array = data_destilled_array[1]
-
-				@master_array.each do |d|
-
-				#data_destilled_array.each do |d|
-
-					@comparator_concepts = d[2].count # a comparator is chosen and its entities counted
-					@comparator_concepts_array = d[2]
-					@comparator_entities = d[1].count # a comparator is chosen and its entities counted
-					@comparator_entities_array = d[1]
-
-					@weight_concepts = 30.0
-					@value_per_concept = 30.0 / @comparator_concepts
-					@weight_entities = 30.0
-					@value_per_entitity = 30.0 / @comparator_entities
-
-					@base_array_concepts.each do |c| # check how many times word exists
-						if @comparator_concepts_array.include? c
-							@weight_concepts = @weight_concepts - @value_per_concept
-						end
-					end
-
-					@base_entities_array.each do |c| # check how many times word exists
-						if @comparator_entities_array.include? c
-							@weight_entities = @weight_entities - @value_per_entitity
-						end
-					end
-
-					d1 = Dataextract.where(:url => data_destilled_array[0]).first.id # base
-					d2 = Dataextract.where(:url => d[0]).first.id # comparator
-					if Difference.exists?(dataextraction1: d1, dataextraction2: d2) || d1 == d2
-					else
-						Difference.create(dataextraction1: d1, dataextraction2: d2, entities_diff: @weight_entities, concepts_diff: @weight_concepts)
-					end
-				#end
-				end
-		end
+	def compare
+		Dataextract.comparison_calculation
 		redirect_to :controller => "dataextracts", :action => "index"
 	end
 
 	def rebase
-		if params[:dataextract].nil?
-			@rebase_id = 1
-		else
-			@rebase_id = params[:dataextract].to_s[/\d+/].to_i
-		end
-
-		@dataextracts = Dataextract.all
-
-    respond_to do |format|
-        format.js {
-          render :template => "dataextracts/compare.js.erb",
-          :layout => false
-        }
-    end
+		@rebase_id = params[:dataextract] ? params[:dataextract].to_s[/\d+/].to_i : 1
+    respond_to do |format| format.js { render :template => "dataextracts/compare.js.erb", :layout => false } end
 	end
 
 	def new
-
 		# REVIEW - ALSO NEED LINKCHECKER
-		demo_url1 = params[:url].to_s.gsub '["', ''
-		demo_url = demo_url1.gsub '"]', ''
+		url1 = params[:url].to_s.gsub '["', ''
+		url = url1.gsub '"]', ''
 
-		if Dataextract.exists?(url: demo_url)
+		if Dataextract.exists?(url: url)
 		else
 			alchemyapi = AlchemyAPI.new()
 
-			myText = "I can't wait to integrate AlchemyAPI's awesome Ruby SDK into my app!"
-			response = alchemyapi.sentiment("text", myText)
-			puts "Sentiment: " + response["docSentiment"]["type"]
-
-
-			entities_hash = Hash.new
-			concepts_hash = Hash.new
-
-			puts '############################################'
-			puts '#   Entity Extraction Example              #'
-			puts '############################################'
-			puts ''
-			puts ''
-
-			puts 'Processing text: ' + demo_url
-			puts ''
-
-			response = alchemyapi.entities('url', demo_url, { 'sentiment'=>1 })
-
+			# ======== Concept Tagging ========
+			keyword_entity_check = Array.new
+			keyword_concept_check = Array.new
+			response = alchemyapi.entities('url', url, { 'sentiment'=>1 })
 			if response['status'] == 'OK'
-				puts '## Response Object ##'
-				#puts JSON.pretty_generate(response)
 				entities_list = JSON.pretty_generate(response)
-
-
-				puts ''
-				puts '## Entities ##'
 				for entity in response['entities']
-					puts 'text: ' + entity['text']
-					puts 'type: ' + entity['type']
-					puts 'relevance: ' + entity['relevance']
-					print 'sentiment: ' + entity['sentiment']['type'] 
-					
-					#Make sure score exists (it's not returned for neutral sentiment
-					if entity['sentiment'].key?('score')
-						print ' (' + entity['sentiment']['score'] + ')'
-					end
-
-					puts ''
+					keyword_entity_check.push(entity['text'])
 				end
 			else
 				puts 'Error in entity extraction call: ' + response['statusInfo']
 			end
 
-
-			puts ''
-			puts ''
-			puts ''
-			puts '############################################'
-			puts '#  Concept Tagging Example                 #'
-			puts '############################################'
-			puts ''
-			puts ''
-
-			puts 'Processing text: ' + demo_url
-			puts ''
-
-			response = alchemyapi.concepts('url', demo_url)
-
+			# ======== Concept Tagging ========
+			response = alchemyapi.concepts('url', url)
 			if response['status'] == 'OK'
-				puts '## Response Object ##'
-				puts JSON.pretty_generate(response)
 				concepts_list = JSON.pretty_generate(response)
-				
-
-				puts ''
-				puts '## Concepts ##'
 				for concept in response['concepts']
-					puts 'text: ' + concept['text']
-					puts 'relevance: ' + concept['relevance']
-					puts ''
+					keyword_concept_check.push(concept['text'])
 				end
 			else
-				#puts 'Error in concept tagging call: ' + response['statusInfo']
+				puts 'Error in concept tagging call: ' + response['statusInfo']
 			end
-			if concepts_list.present? && entities_list.present?
-				Dataextract.create(url: demo_url, concepts_list: concepts_list, entities_list: entities_list)
+
+			if keyword_entity_check.length != 0 && keyword_concept_check.length != 0
+				Dataextract.create(url: url, concepts_list: concepts_list, entities_list: entities_list)
+			elsif keyword_entity_check.length == 0
+				flash[:notice] = "Alchemy could not identify any ENTITY keywords. URL was not added to database!"
+			elsif keyword_concept_check.length == 0
+				flash[:notice] = "Alchemy could not identify any CONCEPT keywords. URL was not added to database!"
 			end
 		end
-
-
 		redirect_to :controller => "dataextracts", :action => "index"
 	end
 
--- app/models/dataextract.rb
+++ app/models/dataextract.rb
@@ -1,2 +1,157 @@
 class Dataextract < ActiveRecord::Base
+
+	def self.keyword_and_url_extraction # Parses the JSON strings from Alchemy stored in the database.
+		@extracted_keywords_and_urls = Hash.new
+		self.all.each do |p|
+			if p.entities_list.present? && p.concepts_list.present? && p.url.present?
+				concept_keywords = Array.new
+				#puts "REVELATOR"
+				#concepts.each {|c| (puts c['text'] + "#{c['relevance']}" )}
+				JSON.parse(p.concepts_list)['concepts'].each {|c| concept_keywords.push(c['text'])}
+				concept_keywords = concept_keywords.uniq # Read more below #1
+
+				entity_keywords = Array.new
+				JSON.parse(p.entities_list)['entities'].each {|e| entity_keywords.push(e['text'])}
+				entity_keywords = entity_keywords.uniq # Read more below #1
+				hash = Hash.new
+				hash = {:url => p.url, :entities => entity_keywords, :concepts => concept_keywords}
+				
+				@extracted_keywords_and_urls[p.id] = hash # pushes hash to master hash
+			end
+		end
+		return @extracted_keywords_and_urls
+	end
+
+	def self.comparison_calculation # Calculates the differences between all articles against all other articles based on entity and concept keywords.
+		extracted_keywords_and_urls = self.keyword_and_url_extraction
+		extracted_keywords_and_urls.each do |base_article_data| # Loops through each article data set to declare as base.
+			base_entities = base_article_data[1][:entities] # a base for entities keywords is chosen.
+			base_concepts = base_article_data[1][:concepts] # a base for concept keywords is chosen.
+
+			extracted_keywords_and_urls.each do |comparator_article_data| # Loops through each article data set to declare as comparator.
+				comparator_entities = comparator_article_data[1][:entities] # a comparator for concepts keywords is chosen.
+				comparator_concepts = comparator_article_data[1][:concepts] # a comparator for entities keywords is chosen.
+
+				total_weight_concepts = 30.0
+				total_weight_entities = 30.0
+				weight_per_concept = total_weight_concepts / comparator_article_data[1][:concepts].count
+				weight_per_entitity = total_weight_entities / comparator_article_data[1][:entities].count
+
+				self.write_console_output_entities(base_article_data[1][:url], base_concepts, comparator_article_data[1][:url], comparator_concepts, total_weight_concepts, weight_per_concept)
+
+				base_concepts.each do |concept_keyword| # check how many times concept keyword exists and detract weight if found.
+					condition = comparator_concepts.include? concept_keyword
+					condition ? (total_weight_concepts = total_weight_concepts - weight_per_concept) : total_weight_concepts
+					condition ? (puts concept_keyword + "   #{total_weight_concepts}") : ""
+				end
+				if total_weight_concepts < 0
+					self.write_console_output_entities_zero(total_weight_concepts)
+					total_weight_concepts = 0
+				end
+
+				self.write_console_output_entities_ending(total_weight_concepts)
+
+				self.write_console_output_entities(base_article_data[1][:url], base_entities, comparator_article_data[1][:url], comparator_entities, total_weight_entities, weight_per_entitity)
+
+				base_entities.each do |entity_keyword| # check how many times concept keyword exists and detract weight if found.
+					condition = comparator_entities.include? entity_keyword
+					condition ? (total_weight_entities = total_weight_entities - weight_per_entitity) : total_weight_entities
+					condition ? (puts entity_keyword + "   #{total_weight_entities}") : ""
+				end
+				if total_weight_entities < 0
+					self.write_console_output_entities_zero(total_weight_entities)
+					total_weight_entities = 0
+				end
+
+				self.write_console_output_entities_ending(total_weight_entities)
+
+				base_article_id = Dataextract.where(:url => base_article_data[1][:url]).first.id
+				comparator_article_id = Dataextract.where(:url => comparator_article_data[1][:url]).first.id
+				if Difference.exists?(dataextraction1: base_article_id, dataextraction2: comparator_article_id) || base_article_id == comparator_article_id
+				else
+					Difference.create(dataextraction1: base_article_id, dataextraction2: comparator_article_id, entities_diff: total_weight_entities, concepts_diff: total_weight_concepts)
+				end
+			end
+		end
+	end
+
+	def self.write_console_output_entities(base_article_url, base_keywords, comparator_article_url, comparator_keywords, total_weight, weight_per_keyword)
+		puts "=================== # CALCULATOR CONCEPTS # ==================="
+		puts ""
+		puts "FIRST URL"
+		puts base_article_url
+		puts ""
+		print base_keywords
+		puts ""
+		puts "Number of entitiy keywords in base:"
+		puts base_keywords.count
+		puts ""
+		puts ""
+		puts "SECOND URL"
+		puts comparator_article_url
+		puts ""
+		print comparator_keywords
+		puts ""
+		puts "Number of concept keywords in comparator:"
+		puts comparator_keywords.count
+		puts ""
+		puts ""
+		puts "Total weight:"
+		puts total_weight
+		puts ""
+		puts "Weight per entity:"
+		puts weight_per_keyword
+		puts ""
+		puts "---------- KEYWORDS MATCHES ----------"
+	end
+
+	def self.write_console_output_entities_zero(total_weight)
+		puts "====== !!!!!! SUB-ZERO !!!!!! ======"
+		puts ""
+		puts total_weight
+		puts ""
+		puts "EQALIZED TO ZERO ===> 0"
+		puts ""
+	end
+
+	def self.write_console_output_entities_ending(total_weight)
+		puts "--------------------------------------"
+		puts ""
+		puts "TOTAL WEIGHT TO INSERT: "
+		puts total_weight
+		puts ""
+		puts ""
+		puts "=================== # END # ==================="
+		puts ""
+		puts ""
+		puts ""
+		puts ""
+		puts ""
+		puts ""
+		puts ""
+	end
+
 end
+
+=begin
+
+========================ISSUES=======================
+
+a. 	For some reason when comparing the same article againt itself it flips out in the last part of the loop.
+		It removes to much weight so it becomes below zero.
+		SUGGESTED SOLUTION: Not calculating the difference between itself is not needed. Ask it to skip.
+
+
+#1 Removes dublicate keywords which Alchemy might output
+
+Alchemy seems to have issues with some links. 
+It puts the exact same keyword several times, but seemt to think it is different things.
+Example https://en.wikipedia.org/wiki/Panama_Papers
+In the example Alchemy says "Mossack Fonseca" is a Person, City, Company and GeographicFeature.
+So the keyword "Mossack Fonseca" appears four times.'
+
+SUGGESTED SOLUTION: 
+Remove duplicates when parsing the JSON string. 
+If deciding later to use relevancy weighting in the calculations, then the first occurence of the word should be used.
+
+=end
